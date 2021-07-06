4a62f506ff540531395deac46a7dc519cb6338f5
seoro-malgm <liass012@gmail.com>
2021-07-07 01:32:03 +0900

    빤쓰런 업로드 완료

--- src/components/Nav/GlobalNav.vue
+++ src/components/Nav/GlobalNav.vue
@@ -15,12 +15,16 @@
             ㅈㄴㅇㄱ
           </span>
         </b-btn>
-        <b-btn variant="text" class="py-0 px-2 rounded-circle" v-b-toggle.info>
+        <b-btn
+          variant="text"
+          class="py-0 px-2 rounded-circle"
+          v-b-toggle.sidebar-about
+        >
           <span class="text-16 d-none d-md-block">
-            INFO
+            ABOUT
           </span>
           <span class="text-14 d-block d-md-none">
-            INFO
+            ABOUT
           </span>
         </b-btn>
         <!-- <b-btn variant="text" class="p-0 rounded-circle">
@@ -28,25 +32,43 @@
     </b-btn> -->
       </nav>
     </transition>
-    <b-sidebar id="info" backdrop right header-class="p-1" body-class="px-2">
+    <b-sidebar
+      id="sidebar-about"
+      backdrop
+      right
+      header-class="p-1"
+      body-class="px-2"
+    >
       <header class="py-2 d-flex align-items-center justify-content-center">
         <h2 class="logo text-24 text-md-48" :class="logoClass(logoIdx)">
           전나이김
         </h2>
       </header>
       <p class="mt-2 py-3">
-        전나이김팀은 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Ipsum ipsam, laborum soluta reprehenderit fugiat quos cum commodi neque?
-        Dignissimos est eaque sapiente obcaecati distinctio ullam rem qui
-        debitis consequuntur quod.
+        전나이김팀은 <strong>전</strong>우주, <strong>나</strong>호정,
+        <strong>이</strong>상준, <strong>김</strong>도현 네명이 한 팀으로
+        '달리기'를 주제를 가지고 각각의 '달리기'를 표현하는 프로젝트를 진행했다.
+        단, '달리기'라는 주제와 임의의 단어를 조합하여 서로 같을 수 없는 주제로
+        선정했다. <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        dicta perferendis quibusdam ab itaque quaerat repellat molestiae eveniet
+        eaque, praesentium ex sequi, consequatur temporibus numquam inventore
+        similique ea veniam at?
       </p>
       <section class="mt-4 mb-3">
-        <h3 class="text-14 text-md-18">
-          연락처? sns 주소??
+        <h3 class="text-14 text-md-18 fw-700">
+          CONTACT
         </h3>
-        <ul class="list-unstyled px-2 mt-2">
-          <li class="mb-3" v-for="i in 4" :key="i">
-            @seoro-malgm
+        <ul class="list-unstyled px-2 mt-1">
+          <li class="mb-3" v-for="(item, i) in sites" :key="i">
+            <b-btn
+              variant="text"
+              class="p-0 fw-700 rounded-circle"
+              target="_blank"
+              :href="item.url"
+            >
+              {{ item.name }}
+            </b-btn>
           </li>
         </ul>
       </section>
@@ -60,6 +82,28 @@ export default {
     return {
       show: false,
       logoIdx: 0,
+      sites: [
+        {
+          name: "@jeonnaleekim.kr",
+          url: "https://www.instagram.com/jeonnaleekim.kr/",
+        },
+        {
+          name: "전우주",
+          url: "https://www.instagram.com/jeonoff/",
+        },
+        {
+          name: "나호정",
+          url: "https://www.instagram.com/hzzxi_aw/",
+        },
+        {
+          name: "이상준",
+          url: "https://www.instagram.com/seoro_malgm/",
+        },
+        {
+          name: "김도현",
+          url: "https://www.instagram.com/avocado_hyun/",
+        },
+      ],
     };
   },
   methods: {
